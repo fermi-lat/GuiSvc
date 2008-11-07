dab3a1fe9afc6dc5e81f9e4f4a24ef2a0f3ab505
ecephas <>
2008-11-07 16:24:36 +0000

    env.Tool('addLibrary', library = env['gaudiLibs'])

--- GuiSvcLib.py
+++ GuiSvcLib.py
@@ -1,8 +1,8 @@
-# $Header: /nfs/slac/g/glast/ground/cvs/GlastRelease-scons/GuiSvc/GuiSvcLib.py,v 1.1 2008/07/09 21:13:42 glastrm Exp $
+# $Header: /nfs/slac/g/glast/ground/cvs/GlastRelease-scons/GuiSvc/GuiSvcLib.py,v 1.2 2008/07/11 00:32:27 glast Exp $
 def generate(env, **kw):
     if not kw.get('depsOnly', 0):
         env.Tool('addLibrary', library = ['GuiSvc'])
     env.Tool('guiLib')
-
+    env.Tool('addLibrary', library = env['gaudiLibs'])
 def exists(env):
     return 1;
