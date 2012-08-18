6114c5f9d80e8e5bf89bf0bc9facf54a9fc38e8d
jrb <>
2012-08-18 00:29:56 +0000

    support no-install headers on Windows

--- GuiSvcLib.py
+++ GuiSvcLib.py
@@ -1,7 +1,9 @@
-# $Header: /nfs/slac/g/glast/ground/cvs/GlastRelease-scons/GuiSvc/GuiSvcLib.py,v 1.2 2008/07/11 00:32:27 glast Exp $
+# $Header: /nfs/slac/g/glast/ground/cvs/GlastRelease-scons/GuiSvc/GuiSvcLib.py,v 1.3 2008/11/07 16:24:36 ecephas Exp $
 def generate(env, **kw):
     if not kw.get('depsOnly', 0):
         env.Tool('addLibrary', library = ['GuiSvc'])
+        if env['PLATFORM']=='win32' and env.get('CONTAINERNAME','')=='GlastRelease':
+	    env.Tool('findPkgPath', package = 'GuiSvc') 
     env.Tool('guiLib')
     env.Tool('addLibrary', library = env['gaudiLibs'])
 def exists(env):
